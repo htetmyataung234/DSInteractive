71ecf2efd573e4991d6656416929886f042e4209
htetmyataung234 <htetmyataungzerozerofour@gmail.com>
2026-03-08 22:48:27 +0630

    Update DS3 Storyline outputs

--- DS3/DS3.3.4 - Storyline output/story_content/user.js
+++ DS3/DS3.3.4 - Storyline output/story_content/user.js
@@ -2,6 +2,39 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6gAeXibQBb4":
+        Script1();
+        break;
+      case "5owLQ9kJza1":
+        Script2();
+        break;
+      case "5ajT7mfgiUI":
+        Script3();
+        break;
   }
+}
+
+function Script1()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script2()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script3()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
 }
 
--- DS3/DS3.4.4 - Storyline output/story_content/user.js
+++ DS3/DS3.4.4 - Storyline output/story_content/user.js
@@ -2,6 +2,105 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6JBYJtbTcBf":
+        Script1();
+        break;
+      case "67nyTe2pRCf":
+        Script2();
+        break;
+      case "5migNDOBQS9":
+        Script3();
+        break;
+      case "62yPO7Z7M5V":
+        Script4();
+        break;
+      case "64Z49lNTP79":
+        Script5();
+        break;
+      case "6p2u5arwBZV":
+        Script6();
+        break;
+      case "5aZWUv7yJiC":
+        Script7();
+        break;
+      case "5cXUTOghB75":
+        Script8();
+        break;
+      case "61gNRqsl4z3":
+        Script9();
+        break;
   }
+}
+
+function Script1()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script2()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script3()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script4()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script5()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script6()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script7()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script8()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
+}
+
+function Script9()
+{
+  var activeEl = document.activeElement;
+if (activeEl.tagName === 'INPUT' || activeEl.tagName === 'TEXTAREA') {
+    activeEl.blur();
+}
 }
 
